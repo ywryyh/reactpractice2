86f7b76ea897939e874bfdac88a660d93eedc74a
yoonho <leynord112@naver.com>
2026-03-13 13:11:56 +0900

    react ing

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BoardList = () => {
+
+    const[posts,setPosts]=useState([])
+    const[currentUser,setCurrentUser]=useState(null)
+
+
+    useEffect(()=>{
+
+    },[])
     return (
         <div>
             
